c650f82101ea4f51241ac79d0d79e1376a86ac75
27bronu <brneto14@gmail.com>
2024-06-22 03:09:36 +0100

    feat: Update API endpoints

--- admin/src/components/ListProduct.jsx
+++ admin/src/components/ListProduct.jsx
@@ -5,7 +5,7 @@ const ListProduct = () => {
   const [allproducts, setAllproducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://fragrance-maxxing-api.onrender.com/allproducts")
       .then((res) => res.json())
       .then((data) => {
         setAllproducts(data);
@@ -17,7 +17,7 @@ const ListProduct = () => {
   }, []);
 
   const remove_product = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://fragrance-maxxing-api.onrender.com/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const login = async () => {
     console.log("Login function executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login", {
+    await fetch("https://fragrance-maxxing-api.onrender.com/login", {
       method: "POST",
       headers: {
         Accept: "application/formData",
@@ -37,7 +37,7 @@ const Login = () => {
   const signup = async () => {
     console.log("signup function executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/signup", {
+    await fetch("https://fragrance-maxxing-api.onrender.com:4000/signup", {
       method: "POST",
       headers: {
         Accept: "application/formData",
